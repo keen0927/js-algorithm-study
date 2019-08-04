d6441af39f1e5ce0ee96f3098971ab714bf0acd3
KEEN <kyh0927@live.co.kr>
2019-08-04 20:38:27 +0900

    190804

---
+++ level1/06-stringarraySort.js
@@ -0,0 +1,12 @@
+// ['abce', 'abcd', 'cdx']와 2가 주어지면 2번째 인덱스 글자(c, c, x)를 기준으로 정렬.
+// 만약 2번째 인덱스 글자가 서로 같다면(c, c처럼) 사전순으로 정렬. 
+
+const strings = ['abce','abcd','cdx'];
+
+function solution(strings, n) {
+    // return strings.sort((p, c) => p[n] === c[n] ? p.localeCompare(c) : p[n].localeCompare(c[n]))
+
+    
+}
+
+console.log(solution(string,2));
