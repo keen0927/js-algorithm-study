82578be7c666979493dcaa88509d9f85daadd76e
keen <kyh0927@within.co.kr>
2019-10-07 18:12:39 +0900

    191007

--- hash/level1.js
+++ hash/level1.js
@@ -19,17 +19,30 @@ const completion = ['josipa', 'filipa', 'marina', 'nikola'];
 
 // console.log(answer);
 
-function solution(participant, completion) {
-    var dic = completion.reduce((obj, t) => (obj[t]= obj[t] ? obj[t]+1 : 1 , obj) ,{});
-    // console.log(dic);
-    return participant.find(t=> {
-        if(dic[t])
-            dic[t] = dic[t]-1;
-        else 
-            return true;
-    });
+// function solution(participant, completion) {
+//     var dic = completion.reduce((obj, t) =>
+//         ( obj[t] = obj[t] ? obj[t]+1 : 1 , obj) ,{}
+//     );
+//     // console.log(dic);
+//     return participant.find(t=> {
+//         if(dic[t])
+//             dic[t] = dic[t]-1;
+//         else 
+//             return true;
+//     });
+// }
+
+// var ddd = solution(participant, completion);
+
+// console.log(ddd);
+
+const solution = (participant, completion) => {
+    participant.sort();
+    completion.sort();
+
+    for (i = 0; i < participant.length ; i++) {
+        if (participant[i] !== completion[i]) return participant[i];
+    }
 }
 
-var ddd = solution(participant, completion);
-
-console.log(ddd);
+console.log(solution(participant, completion));
