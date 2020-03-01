042208c0838195635ffdd37d424df53677f85cfc
KEEN <kyh0927@live.co.kr>
2020-03-01 23:40:30 +0900

    20200301

---
+++ algorithms/insertSort_Re.js
@@ -0,0 +1,36 @@
+const arr = [1,3,5,2];
+
+// function insertionSort(arr) {
+//   for ( let i = 1 ; i < arr.length ; i++ ) {
+//     let current = arr[i];
+//     let j = i - 1;
+  
+//     while(j >= 0 && arr[j] > current) {  
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
+
+//     arr[j + 1] = current;
+//   }
+//   console.log(arr);
+// }
+
+// const insertionSort = (arr) => {
+//   for( let i = 1; i < arr.length ; i++) {
+//     let current = arr[i];
+//     let j = i - 1;
+
+//     while (j >= 0 && arr[j] > current) {
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
+
+//     arr[j + 1] = current;
+
+//   }
+
+//   console.log(arr);
+// };
+
+
+// insertionSort(arr);
